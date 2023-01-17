f3d829538095c45c0356fd749ea5b32024fc5946
Kasra Keshvardoost <kasrakeshvardoost@gmail.com>
2023-01-18 02:28:12 +0330

    deleted the return on refresh endpoint

--- server/routes/auth/refresh.post.js
+++ server/routes/auth/refresh.post.js
@@ -3,6 +3,7 @@ import { setCookie } from "h3";
 
 const getAuthToken = async (req, data) => {
     const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress || null;
+
     delete req.headers["content-length"];
     delete req.headers["host"];
 
@@ -24,12 +25,23 @@ export default defineEventHandler(async (event) => {
     const { req, res } = event.node;
     let maxAge = 3600;
 
+    // ====================
+    // TODO : token is empty and therefore is not setting... need to be fixed
+    // works fine on localhost but not on vercel and railway
+    // tested the api endpoint on railway btw... seems to be working fine
+
+    // results of some digging:
+    // seems that token can't be verified becuse eveytime system on railway redeploys it deletes old file including images
+    // and pem keys... so the tokens cant be verified... if we want to continue with this we have 2 options:
+    // 1 - no more refresh tokens
+    // 2 - no more verfication with pem keys
+    // ====================
+
     // request the back-end and set new auth-token
-    const token = await getAuthToken(req, {});
+    const token = await getAuthToken(req, { profile: "test", email: "test" });
     // if (!token) maxAge = 0;
 
     if (!!token) setCookie(event, "AuthToken", token, { sameSite: "strict", path: "/", httpOnly: true, secure: true, maxAge: maxAge });
 
-    // return res.end();
-    return res.end(JSON.stringify({ token }));
+    // return res.end(JSON.stringify({ token }));
 });
